396ccdf4919c87e2acc75308836bd0c9aabb8492
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-08-23 00:14:03 +0530

    Added null checks

--- js/calcHypotenuse.js
+++ js/calcHypotenuse.js
@@ -6,6 +6,19 @@ var message = document.querySelector("#message");
 submit.addEventListener("click", clickHandler)
 
 function clickHandler() {
-    let hypotenuse = Math.sqrt(Math.pow(base.value, 2) + Math.pow(height.value, 2))
+
+    var b = base.value;
+    var h = height.value;
+
+    //Null check
+    if (!(b) || !(h)) {
+        alert("Enter all neccessary values")
+        return
+    } else if (b <= 0 || h <= 0) { //Negative values
+        alert("Enter proper values")
+        return
+    }
+
+    let hypotenuse = Math.sqrt(Math.pow(b, 2) + Math.pow(h, 2))
     message.innerText = "The length of hypotenuse is " + hypotenuse
 }
--- js/isTriangle.js
+++ js/isTriangle.js
@@ -3,7 +3,19 @@ var button = document.querySelector("#submit")
 var output = document.querySelector("#message")
 
 button.addEventListener("click", function check() {
-    let sum = Number(angles[0].value) + Number(angles[1].value) + Number(angles[2].value)
+    var angle1 = angles[0].value;
+    var angle2 = angles[1].value;
+    var angle3 = angles[2].value;
+    //Null check
+    if (!(angle1) || !(angle2) || !(angle3)) {
+        alert("Enter all neccessary values")
+        return
+    } else if (angle1 <= 0 || angle2 <= 0 || angle3 <= 0) { //Negative values
+        alert("Enter proper values")
+        return
+    }
+
+    let sum = Number(angle1) + Number(angle2) + Number(angle3)
     if (sum === 180) {
         showMessage(" YAYYY.... The angles form a triangle");
     } else {
